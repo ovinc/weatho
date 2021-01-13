6f4ec2ad634b4426b1d0b0c852815145c98bb908
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2021-01-13 10:50:55 +0100

    Define __version__ automatically in __init__.py; darksky.py -> weather.py

--- weatherov/__init__.py
+++ weatherov/__init__.py
@@ -1,5 +1,9 @@
-from .darksky import generate_url, generate_filename
-from .darksky import weather_pt, weather_day, weather_days
-from .darksky import download_day, download_days, load_day
-from .darksky import check_missing_days, download_missing_days
-from .darksky import weather_plot
+from .weather import generate_url, generate_filename
+from .weather import weather_pt, weather_day, weather_days
+from .weather import download_day, download_days, load_day
+from .weather import check_missing_days, download_missing_days
+from .weather import weather_plot
+
+from importlib.metadata import version
+
+__version__ = version('weatherov')
